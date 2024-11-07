5e8e5ce77c76935e841584f7240ab9f33e51dc92
Vladykkk <rubakvlad7@gmail.com>
2024-11-07 22:14:45 +0200

    refactored lab 9, and 2

--- lab_2/main.py
+++ lab_2/main.py
@@ -1,5 +1,7 @@
 from ui.console_ui import ConsoleUI
 
-if __name__ == "__main__":
-    ui = ConsoleUI()
-    ui.run()
+class Lab2Runner:
+    def run(self):
+        print("Запуск Лабораторної роботи №2...")
+        ui = ConsoleUI()
+        ui.run()
--- lab_9/main.py
+++ lab_9/main.py
@@ -1,5 +1,34 @@
-from runner import Runner
+from lab_2.main import Lab2Runner
+
+class RunnerFacade:
+    def __init__(self):
+        self.labs = {
+            "2": Lab2Runner(),
+        }
+
+    def run_lab(self, lab_number):
+        lab = self.labs.get(lab_number)
+        if lab:
+            lab.run()
+        else:
+            print("Невірний номер лабораторної роботи.")
+
+    def display_menu(self):
+        print("\nМеню запуску лабораторних робіт:")
+        for number in self.labs.keys():
+            print(f"Лабораторна робота №{number}")
+        print("Введіть номер лабораторної роботи або 'q' для виходу.")
+
+    def start(self):
+        while True:
+            self.display_menu()
+            choice = input("Ваш вибір: ")
+            if choice.lower() == 'q':
+                print("Вихід з програми.")
+                break
+            else:
+                self.run_lab(choice)
 
 if __name__ == "__main__":
-    runner = Runner()
-    runner.run()
+    runner = RunnerFacade()
+    runner.start()
